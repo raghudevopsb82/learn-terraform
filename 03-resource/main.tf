7cdf522431be9d18c34e4de74569948121f33d82
Raghu K <raghuk.vit@gmail.com>
2024-12-20 06:40:07 +0530

    RoboSHop Project

--- 03-resource/main.tf
+++ 03-resource/main.tf
@@ -16,7 +16,7 @@ resource "azurerm_virtual_machine" "main" {
   location              = data.azurerm_resource_group.example.location
   resource_group_name   = data.azurerm_resource_group.example.name
   network_interface_ids = [azurerm_network_interface.example.id]
-  vm_size               = "Standard B2s"
+  vm_size               = "Standard_DS1_v2"
 
   # Uncomment this line to delete the OS disk automatically when deleting the VM
   delete_os_disk_on_termination = true
