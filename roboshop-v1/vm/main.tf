1d20c7382df086c0c0d4e0dd54b608f9619a5f1b
SVR Kalluru <raghuk.vit@gmail.com>
2024-12-23 06:19:56 +0530

    RoboShop

--- roboshop-v1/vm/main.tf
+++ roboshop-v1/vm/main.tf
@@ -15,7 +15,7 @@ resource "azurerm_public_ip" "main" {
   name                = "${var.component}-ip"
   location              = data.azurerm_resource_group.example.location
   resource_group_name   = data.azurerm_resource_group.example.name
-  allocation_method   = "Dynamic"
+  allocation_method   = "Static"
 
   tags = {
     component = var.component
