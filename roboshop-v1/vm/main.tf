dd53aeccd5b5436c23fa77c736874089ece6ab61
SVR Kalluru <raghuk.vit@gmail.com>
2024-12-24 06:23:00 +0530

    RoboShop

--- roboshop-v1/vm/main.tf
+++ roboshop-v1/vm/main.tf
@@ -84,23 +84,6 @@ resource "azurerm_virtual_machine" "main" {
   tags = {
     component = var.component
   }
-
-
-#   provisioner "remote-exec" {
-#
-#     connection {
-#       type     = "ssh"
-#       user     = "testadmin"
-#       password = "Password1234!"
-#       host     = azurerm_public_ip.main.ip_address
-#     }
-#
-#     inline = [
-#       "sudo dnf install python3.12-pip -y",
-#       "sudo pip3.12 install ansible",
-#       "ansible-pull -i localhost, -U https://github.com/raghudevopsb82/roboshop-ansible roboshop.yml -e app_name=${var.component} -e ENV=dev"
-#     ]
-#   }
 }
 
 resource "azurerm_dns_a_record" "main" {
@@ -111,7 +94,24 @@ resource "azurerm_dns_a_record" "main" {
   records             = [azurerm_network_interface.main.private_ip_address]
 }
 
+resource "null_resource" "ansible" {
 
+    provisioner "remote-exec" {
+
+      connection {
+        type     = "ssh"
+        user     = "testadmin"
+        password = "Password1234!"
+        host     = azurerm_public_ip.main.ip_address
+      }
+
+      inline = [
+        "sudo dnf install python3.12-pip -y",
+        "sudo pip3.12 install ansible",
+        "ansible-pull -i localhost, -U https://github.com/raghudevopsb82/roboshop-ansible roboshop.yml -e app_name=${var.component} -e ENV=dev"
+      ]
+    }
+}
 
 
 
