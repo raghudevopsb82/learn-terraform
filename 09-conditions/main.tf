7faf460b5f20efb3357ac51cc4522d3b0197acc8
Raghu K <raghuk.vit@gmail.com>
2024-12-30 06:19:23 +0530

    RoboSHop Project

--- 09-conditions/main.tf
+++ 09-conditions/main.tf
@@ -10,4 +10,14 @@ resource "null_resource" "test" {
   count = var.a > 10 ? 1 : 0
 }
 
+# if the above expression var.a > 10 ? 1 : 0, lets say it needs to be used in 10 different places, tomorrow if we want to change the expression, it becomes a duplicate work to change in 10 places, to keep the code dry, terraform supports local resources. Which is like run time variables.
+
+locals {
+  count = var.a > 10 ? 1 : 0
+}
+
+resource "null_resource" "test1" {
+  count = local.count
+}
+
 
