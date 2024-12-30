82a7fc4896167de4b91f68cf41e05ac4cf30d2bb
Raghu K <raghuk.vit@gmail.com>
2024-12-30 06:15:41 +0530

    RoboSHop Project

--- 09-conditions/main.tf
+++ 09-conditions/main.tf
@@ -4,5 +4,10 @@ output "test" {
   value = var.a > 10 ? "a is greater than 10" : "a is lesser than 10"
 }
 
+# these condtions with combination of loop can be used to determine whether a resource to run or not.
+
+resource "null_resource" "test" {
+  count = var.a > 10 ? 1 : 0
+}
 
 
