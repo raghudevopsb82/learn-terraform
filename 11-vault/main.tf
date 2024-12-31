3af161ccb3c8eec765b3e8d835cc86c25591b2a4
Raghu K <raghuk.vit@gmail.com>
2024-12-31 07:15:01 +0530

    RoboSHop Project

---
+++ 11-vault/main.tf
@@ -0,0 +1,22 @@
+provider "vault" {
+  # It is strongly recommended to configure this provider through the
+  # environment variables described above, so that each user can have
+  # separate credentials set in the environment.
+  #
+  # This will default to using $VAULT_ADDR
+  # But can be set explicitly
+  address = "http://vault.azdevopsb82.online:8200"
+  token = var.token
+}
+
+variable "token" {}
+
+data "vault_kv_secret" "secret_data" {
+  path = "test/demo-ssh"
+}
+
+
+
+
+
+
